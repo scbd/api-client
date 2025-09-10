8ac1437791622cfe460f42b011a238f41496b7ca
Mathieu Desjarlais <github@desmat.ca>
2025-09-10 17:20:00 -0400

    rm dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ApiBase, ApiError } from "./api-base";
-import { ThingApi } from "./things";
-import { GaiaApi } from "./gaia";
-export { ApiBase, ApiError, ThingApi, GaiaApi };
--- dist/things.d.ts
+++
@@ -1,5 +0,0 @@
-import { ApiBase } from './api-base';
-export declare class ThingApi extends ApiBase {
-    constructor(opts: any);
-    getThings(page?: number): Promise<any>;
-}
