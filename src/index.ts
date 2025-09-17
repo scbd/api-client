2091e454e7984c8e18aeef36f6716b2f3afb0119
Mathieu Desjarlais <github@desmat.ca>
2025-09-17 15:53:36 -0400

    don't export ApiBase as default

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-import ApiBase from "./api-base.js";
-export {default as ApiError } from "./api-error.js";
-
-export default ApiBase;
+export { default as ApiBase } from "./api-base.js";
+export { default as ApiError } from "./api-error.js";
