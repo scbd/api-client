a244cc5b8e759e045a707da2a626260806c254ab
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2025-09-24 13:24:34 -0400

    Defines a type for localized strings (#4)
    * Defines a type for localized strings
    
    Introduces a new type definition for localized strings (`lstring`).
    
    The type enforces valid keys composed of two or three lowercase letters, aiming to represent language or locale codes. This improves type safety and provides a structured way to manage localized content within the application.
    
    * Makes lstring properties required
    
    Ensures all properties within the `lstring` type definition are mandatory.
    This prevents potential issues caused by missing values.
    
    * Refactors type definition for clarity
    
    Updates the type definition for lstring to use a more descriptive name for the locale key.
    
    This improves code readability and maintainability by clarifying the purpose of the key within the lstring type.

--- src/types/lstring.ts
+++ src/types/lstring.ts
@@ -1,3 +1,11 @@
-export default interface LString {
-    [locale: string]: string
+type Letter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
+
+type TwoLetterKey = `${Letter}${Letter}`
+type ThreeLetterKey = `${Letter}${Letter}${Letter}`
+type ValidKey = TwoLetterKey | ThreeLetterKey
+
+type lstring = {
+  [locale in ValidKey]: string
 }
+
+export default lstring;
