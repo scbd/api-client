b29801055d3a745475532ff64a6a319cea09b36a
Mathieu Desjarlais <github@desmat.ca>
2025-10-29 10:51:48 -0400

    review; delete example api (we have countries now)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-// export shared types 
+// export shared types
 export type { default as LString } from './types/lstring.js';
 export type { default as MongoFilters } from './types/mongo-filters.js';
+
+// export api clients
+export { default as CountriesApi } from './clients/countries.js';
