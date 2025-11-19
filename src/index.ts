9d82577e164ccbb170c38b215e6f1d59994b3dfe
Mathieu Desjarlais <github@desmat.ca>
2025-11-19 16:18:17 -0500

    forgot about the index

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export type { default as MongoFilters } from './types/mongo-filters.js';
 
 // export api clients
 export { default as CountriesApi } from './clients/countries.js';
+export { default as TemporaryFilesApi } from './clients/temporary-files.js';
